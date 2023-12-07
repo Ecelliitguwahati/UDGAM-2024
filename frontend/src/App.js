a6d34354c587e95813bb545ce35fb0467d20b268
krish <krishmangal2@gmail.com>
2023-12-07 20:43:02 +0530

    UDGAM-2024

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Navbar';
+import './WhIsUdgam';
+import Udgam from './WhIsUdgam';
+import Merchandise from './Merchandise';
+import Sponsers from './Spnsers';
+ 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="HomePage">
+      <Navbar />;
+       <Udgam />;
+       <Merchandise />;
+       <Sponsers />;
+      </div>
+      
     </div>
   );
 }
---
+++ frontend/src/Spnsers.js
@@ -0,0 +1,17 @@
+import coinpurple from './Coin Purple.png'
+
+const Sponsers = () => {
+    return ( 
+        <div className="Sponsers">
+            <div className="spon">
+            <h1 className="sponText">Our Sponsers</h1>
+            <img src={coinpurple} alt="coinpurple" className="coinpurple" />
+            </div>
+             <div className="SponList">
+                
+             </div>
+        </div>
+     );
+}
+ 
+export default Sponsers;
