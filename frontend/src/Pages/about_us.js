aea6796da8022cc627ae3c98375461caa1819197
Shivam Agrawal <shivam.agrawal@iitg.ac.in>
2024-01-03 16:13:17 +0530

    added heads img

--- frontend/src/Pages/about_us.js
+++ frontend/src/Pages/about_us.js
@@ -1,6 +1,21 @@
 import "../styles/about_us.css"
 import headphoto from "../images/headphoto.svg";
 import teamphoto from "../images/teamsphoto.svg";
+import aman from "../images/team/aman.jpg";
+import aniba from "../images/team/aniba.jpeg";
+import ashish from "../images/team/ashish.jpeg";
+import dushyanth from "../images/team/dushyanth.jpeg";
+import harsh from "../images/team/harsh.jpeg";
+import kavinash from "../images/team/kavinash.jpg";
+import khushi from "../images/team/khushi.jpeg";
+import lakshya from "../images/team/lakshya.jpeg";
+import netra from "../images/team/netra.jpg";
+import nityam from "../images/team/nityam.jpg";
+import parth from "../images/team/parth.jpeg";
+import shivam from "../images/team/shivam.jpeg";
+import sneha from "../images/team/sneha.jpeg";
+import yash from "../images/team/yash.jpeg";
+
 export const About_us = () => {
 
     return (
@@ -8,84 +23,98 @@ export const About_us = () => {
             <div className="abouthead">MEET THE TEAM</div>
             <div className="aboutgrid">
                 <div className="headtags">
-                    <img src={headphoto} alt="mike" className="aboutimg" />
+                    <img src={aman} alt="mike" className="aboutimg" />
                     <div className="headdetails">
                         <p className="headname">Vedaprakash Rajan</p>
                         <p className="headrank">Design head</p>
                     </div>
                 </div>
                 <div className="headtags">
-                    <img src={headphoto} alt="mike" className="aboutimg" />
+                    <img src={aniba} alt="mike" className="aboutimg" />
                     <div className="headdetails">
                         <p className="headname">Vedaprakash Rajan</p>
                         <p className="headrank">Design head</p>
                     </div>
                 </div>
                 <div className="headtags">
-                    <img src={headphoto} alt="mike" className="aboutimg" />
+                    <img src={ashish} alt="mike" className="aboutimg" />
                     <div className="headdetails">
                         <p className="headname">Vedaprakash Rajan</p>
                         <p className="headrank">Design head</p>
                     </div>
                 </div>
                 <div className="headtags">
-                    <img src={headphoto} alt="mike" className="aboutimg" />
+                    <img src={dushyanth} alt="mike" className="aboutimg" />
                     <div className="headdetails">
                         <p className="headname">Vedaprakash Rajan</p>
                         <p className="headrank">Design head</p>
                     </div>
                 </div>
                 <div className="headtags">
-                    <img src={headphoto} alt="mike" className="aboutimg" />
+                    <img src={harsh} alt="mike" className="aboutimg" />
                     <div className="headdetails">
                         <p className="headname">Vedaprakash Rajan</p>
                         <p className="headrank">Design head</p>
                     </div>
                 </div>
                 <div className="headtags">
-                    <img src={headphoto} alt="mike" className="aboutimg" />
+                    <img src={kavinash} alt="mike" className="aboutimg" />
                     <div className="headdetails">
                         <p className="headname">Vedaprakash Rajan</p>
                         <p className="headrank">Design head</p>
                     </div>
                 </div>
                 <div className="headtags">
-                    <img src={headphoto} alt="mike" className="aboutimg" />
+                    <img src={khushi} alt="mike" className="aboutimg" />
                     <div className="headdetails">
                         <p className="headname">Vedaprakash Rajan</p>
                         <p className="headrank">Design head</p>
                     </div>
                 </div>
                 <div className="headtags">
-                    <img src={headphoto} alt="mike" className="aboutimg" />
+                    <img src={lakshya} alt="mike" className="aboutimg" />
                     <div className="headdetails">
                         <p className="headname">Vedaprakash Rajan</p>
                         <p className="headrank">Design head</p>
                     </div>
                 </div>
                 <div className="headtags">
-                    <img src={headphoto} alt="mike" className="aboutimg" />
+                    <img src={netra} alt="mike" className="aboutimg" />
                     <div className="headdetails">
                         <p className="headname">Vedaprakash Rajan</p>
                         <p className="headrank">Design head</p>
                     </div>
                 </div>
                 <div className="headtags">
-                    <img src={headphoto} alt="mike" className="aboutimg" />
+                    <img src={nityam} alt="mike" className="aboutimg" />
                     <div className="headdetails">
                         <p className="headname">Vedaprakash Rajan</p>
                         <p className="headrank">Design head</p>
                     </div>
                 </div>
                 <div className="headtags">
-                    <img src={headphoto} alt="mike" className="aboutimg" />
+                    <img src={parth} alt="mike" className="aboutimg" />
                     <div className="headdetails">
                         <p className="headname">Vedaprakash Rajan</p>
                         <p className="headrank">Design head</p>
                     </div>
                 </div>
                 <div className="headtags">
-                    <img src={headphoto} alt="mike" className="aboutimg" />
+                    <img src={shivam} alt="mike" className="aboutimg" />
+                    <div className="headdetails">
+                        <p className="headname">Vedaprakash Rajan</p>
+                        <p className="headrank">Design head</p>
+                    </div>
+                </div>
+                <div className="headtags">
+                    <img src={sneha} alt="mike" className="aboutimg" />
+                    <div className="headdetails">
+                        <p className="headname">Vedaprakash Rajan</p>
+                        <p className="headrank">Design head</p>
+                    </div>
+                </div>
+                <div className="headtags">
+                    <img src={yash} alt="mike" className="aboutimg" />
                     <div className="headdetails">
                         <p className="headname">Vedaprakash Rajan</p>
                         <p className="headrank">Design head</p>
