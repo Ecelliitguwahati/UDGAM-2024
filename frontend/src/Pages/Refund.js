eb9cbf6ce5ac5a088172141d6dc29915f308452d
aryapandeyy22 <aryapandeyy975@gmail.com>
2024-01-04 16:39:22 +0530

    changes in t&c,privacy and refund

--- frontend/src/Pages/Refund.js
+++ frontend/src/Pages/Refund.js
@@ -4,15 +4,16 @@ import '../styles/termsandconds.css'
 function Refund() {
   return (
     
-        <div className="containerterm" style={{height:"100vh"}}>
+        <div className="containerterm" style={{height:"60vh"}}>
   <p className="termheading"><strong>Refund Policy- UDGAM 2023</strong></p>
 
-
+<div className="texts">
 <p><span style={{fontWeight:"400"}}>UDGAM does not facilitate availing of any refund.
  We take your registration in the event as your will and consent to participate and therefore amount paid by you is used in the facilitation of conduction of the event.
 Thus, we do not refund any amount paid to us under any circumstance.
 </span></p>
-<p><br /><br /><br /><br /></p>
+<p><br /><br /><br /></p>
+</div>
   </div>
     
   );
