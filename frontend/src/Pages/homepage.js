ad702d2b807d78e336b886ae78cf88bc3c75c8b0
Shivam Agrawal <shivam.agrawal@iitg.ac.in>
2024-01-05 00:09:04 +0530

    jai sneha

--- frontend/src/Pages/homepage.js
+++ frontend/src/Pages/homepage.js
@@ -43,7 +43,7 @@ export const Home = () => {
                     <Events />
                     <Speakerspage />
                     <Merchandise />
-                    <Sponsers />
+                    {/* <Sponsers /> */}
                     {/* <FAQs /> */}
                 </>
             )}
